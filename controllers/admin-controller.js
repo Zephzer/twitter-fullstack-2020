fc168be5cd8ee51a640d42d013087c060c8cacf4
lilymichael87@gmail.com <lilymichael87@gmail.com>
2023-08-24 23:31:35 +0800

    add tweets page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,3 +1,14 @@
-const adminController = {}
+const { Tweet, User, Reply, Like, Followship } = require('../models')
+const adminController = {
+    getTweets: (req, res, next) => {
+        Tweet.findAll({
+            include: [User],
+            raw: true,
+            nest: true
+        })
+        .then(tweets => res.render('admin/tweets', tweets))
+        .catch(err => next(err))
+    }
+}
 
 module.exports = adminController
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
+const { generalErrorHandler } = require('../../middleware/error-handler')
 
+router.get('/tweets', adminController.getTweets)
+
+router.use('/', (req, res) => res.redirect('/admin/tweets'))
+router.use('/', generalErrorHandler)
 
 module.exports = router
