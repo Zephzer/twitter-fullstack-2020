0e1a0b3eb110be9ecdc77e0c40d43f0fd618c9c0
lilymichael87@gmail.com <lilymichael87@gmail.com>
2023-08-27 00:23:11 +0800

    feat: admin user page done

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,6 @@
 const { Tweet, User, Like } = require('../models')
+const { Op } = require('sequelize')
+const controllerHelper = require('../helpers/controller-helpers')
 const adminController = {
     getTweets: (req, res, next) => {
         Tweet.findAll({
@@ -6,37 +8,37 @@ const adminController = {
             raw: true,
             nest: true
         })
-        .then(tweets => {
-            const maxLength = 50
-            tweets = tweets.map(tweet => ({
-                ...tweet,
-                description: tweet.description.slice(0, maxLength)
-            }))
-            res.render('admin/tweets', { tweets })
-        })
-        .catch(err => next(err))
+            .then(tweets => {
+                const maxLength = 50
+                tweets = tweets.map(tweet => ({
+                    ...tweet,
+                    description: tweet.description.slice(0, maxLength)
+                }))
+                res.render('admin/tweets', { tweets })
+            })
+            .catch(err => next(err))
     },
     getUsers: (req, res, next) => {
-        return Promise.all([
-            User.findAll({
-            include: [{ model: Tweet }, { model: User, as: 'Followers' }, { model: User, as: 'Followings' }],
-            where: { role: 'user' },
-            nest: true
-            }),
-            Like.findAll
-    ])
-
-        
-        .then(users => {
-            users = users.map(user => ({
-                ...user.toJSON(),
-                tweetsCount: user.Tweets.length,
-                Followers: user.Followers.length,
-                Followings: user.Followings.length
-            }))
-            console.log(users)
-            res.render('admin/users', { users })
-        })
+        User.findAll({
+            include: [{ model: Tweet, include: Like }, { model: User, as: 'Followers' }, { model: User, as: 'Followings' }],
+            where: { [Op.or]: [{ role: 'user' }, { role: null }] }
+        }
+        )
+            .then(users => {
+                console.log(users)
+                return users.map(user => ({
+                    ...user.toJSON(),
+                    tweetsCount: controllerHelper.countTweets(user.Tweets.length),
+                    Followers: user.Followers.length,
+                    Followings: user.Followings.length,
+                    likesCount: controllerHelper.countLikes(user, user.Tweets.length)
+                }))
+            })
+            .then(users => {
+                console.log(users)
+                res.render('admin/users', { users })
+            })
+            .catch(err => next(err))
     }
 }
 
