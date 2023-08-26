36d87f4ab7e103b6cc5419c2338a98941d075713
lilymichael87@gmail.com <lilymichael87@gmail.com>
2023-08-26 19:19:43 +0800

    commit for merge

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,4 @@
-const { Tweet, User, Reply, Like, Followship } = require('../models')
+const { Tweet, User, Like } = require('../models')
 const adminController = {
     getTweets: (req, res, next) => {
         Tweet.findAll({
@@ -10,18 +10,33 @@ const adminController = {
             const maxLength = 50
             tweets = tweets.map(tweet => ({
                 ...tweet,
-                description: tweet.description.slice(0, 50)
+                description: tweet.description.slice(0, maxLength)
             }))
             res.render('admin/tweets', { tweets })
         })
         .catch(err => next(err))
     },
     getUsers: (req, res, next) => {
-        const id = req.user.id
-        User.findByPk(id, {
-            raw: true,
+        return Promise.all([
+            User.findAll({
+            include: [{ model: Tweet }, { model: User, as: 'Followers' }, { model: User, as: 'Followings' }],
+            where: { role: 'user' },
+            nest: true
+            }),
+            Like.findAll
+    ])
+
+        
+        .then(users => {
+            users = users.map(user => ({
+                ...user.toJSON(),
+                tweetsCount: user.Tweets.length,
+                Followers: user.Followers.length,
+                Followings: user.Followings.length
+            }))
+            console.log(users)
+            res.render('admin/users', { users })
         })
-        .then(user => res.render('admin/users', { user }))
     }
 }
 
